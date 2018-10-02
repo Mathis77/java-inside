734fd431c91fe2a8513c690dec14a8d39894a83f
RYBCZYNSKI MATHIS <mrybczyn@localhost.localdomain>
2018-10-02 10:58:55 +0200

    ajout de la méthode intSwitch2

--- LabTwo/src/main/java/fr.esipe.javainside.labtwo/fr/esipe/javainside/labtwo/Switches.java
+++ LabTwo/src/main/java/fr.esipe.javainside.labtwo/fr/esipe/javainside/labtwo/Switches.java
@@ -1,5 +1,15 @@
 package fr.esipe.javainside.labtwo;
 
+/*
+ * Le code du switch en bytecode indique à la machine virtuelle des "goto" en fonction des "case" du switch.
+ * Par exemple : 
+ * 		tableswitch {
+ * 			0: <ligne instruction pour le cas 0>
+ * 			1 : <ligne instruction pour le cas 1>
+ * 			...
+ * 		}
+ */
+
 public final class Switches {
 	
 	public static String intSwitch(int n) {
@@ -8,6 +18,15 @@ public final class Switches {
 		case 1: return "one";
 		case 2: return "a lot";
 		default: throw new IllegalArgumentException("You need to specified a positiv number !");
+		}
+	}
+	
+	public static String intSwitch2(int n) {
+		switch(n) {
+		case 0: return "zero";
+		case 10: return "ten";
+		case 100: return "a lot";
+		default: throw new IllegalArgumentException("You need to specified either 0, 10 or 100 !");
 		}
 	}
 
