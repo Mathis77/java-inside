d9adf23bab3b87ad4f89152e7e926e744e856371
Mathis <ryb.mathis@hotmail.fr>
2018-10-07 13:35:18 +0200

    commit

--- LabOne/src/main/java/fr.esipe.javainside.labone/fr/esipe/javainside/labone/Main.java
+++ LabOne/src/main/java/fr.esipe.javainside.labone/fr/esipe/javainside/labone/Main.java
@@ -1,28 +1,22 @@
 package fr.esipe.javainside.labone;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 public class Main {
 
 	private final static long NUMBER_OF_ITERATIONS = 10_000_000;
 
 	public static void main(String[] args) throws RunnerException {
-		Options opt = new OptionsBuilder().include(Main.class.getSimpleName()).forks(1).build();
-
-		new Runner(opt).run();
+//		Options opt = new OptionsBuilder().include(Main.class.getSimpleName()).forks(1).build();
+//
+//		new Runner(opt).run();
 	}
 
-	@Benchmark
-	@BenchmarkMode(Mode.SampleTime)
-	public void loopSumPerf() {
-		Sums.loopSum(NUMBER_OF_ITERATIONS);
-	}
+//	@Benchmark
+//	@BenchmarkMode(Mode.SampleTime)
+//	public void loopSumPerf() {
+//		// Sums.loopSum(NUMBER_OF_ITERATIONS);
+//	}
 
 //	@Benchmark
 //	@BenchmarkMode(Mode.SampleTime)
